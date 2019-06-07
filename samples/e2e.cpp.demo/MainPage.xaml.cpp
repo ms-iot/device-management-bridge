ec81fa0f9b4cf521aa9ca625d3b4607604565716
George Mileka <gmileka@microsoft.com>
2019-06-06 20:22:22 -0700

    Added sample cpp app that reads TPM DPS registration information and converts it into a QR code to be displayed.

---
+++ samples/e2e.cpp.demo/MainPage.xaml.cpp
@@ -0,0 +1,145 @@
+﻿/*
+Copyright 2018 Microsoft
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+and associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH
+THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#include "pch.h"
+#include "MainPage.xaml.h"
+#include "..\..\deps\QR-Code-generator\cpp\BitBuffer.hpp"
+#include "..\..\deps\QR-Code-generator\cpp\QrCode.hpp"
+
+using std::uint8_t;
+using qrcodegen::QrCode;
+using qrcodegen::QrSegment;
+
+using namespace e2e_cpp_demo;
+
+using namespace Platform;
+using namespace Windows::Foundation;
+using namespace Windows::Foundation::Collections;
+using namespace Windows::UI::Xaml;
+using namespace Windows::UI::Xaml::Controls;
+using namespace Windows::UI::Xaml::Controls::Primitives;
+using namespace Windows::UI::Xaml::Data;
+using namespace Windows::UI::Xaml::Input;
+using namespace Windows::UI::Xaml::Media;
+using namespace Windows::UI::Xaml::Media::Imaging;
+using namespace Windows::UI::Xaml::Navigation;
+
+using namespace std;
+
+using namespace DMBridgeComponent;
+
+wstring MultibyteToWide(const char* s)
+{
+    size_t length = s ? strlen(s) : 0;
+    size_t requiredCharCount = MultiByteToWideChar(CP_UTF8, 0, s, static_cast<int>(length), nullptr, 0);
+
+    // add room for \0
+    ++requiredCharCount;
+
+    vector<wchar_t> wideString(requiredCharCount);
+    MultiByteToWideChar(CP_UTF8, 0, s, static_cast<int>(length), wideString.data(), static_cast<int>(wideString.size()));
+
+    return wstring(wideString.data());
+}
+
+string WideToMultibyte(const wchar_t* s)
+{
+    size_t length = s ? wcslen(s) : 0;
+    size_t requiredCharCount = WideCharToMultiByte(CP_UTF8, 0, s, static_cast<int>(length), nullptr, 0, nullptr, nullptr);
+
+    // add room for \0
+    ++requiredCharCount;
+
+    vector<char> multibyteString(requiredCharCount);
+    WideCharToMultiByte(CP_UTF8, 0, s, static_cast<int>(length), multibyteString.data(), static_cast<int>(multibyteString.size()), nullptr, nullptr);
+
+    return string(multibyteString.data());
+}
+
+MainPage::MainPage()
+{
+	InitializeComponent();
+}
+
+void MainPage::DpsEnrollmentInfo::Load()
+{
+    TpmBridge^ tpmBridge = ref new TpmBridge();
+
+    registrationId = tpmBridge->GetRegistrationId();
+    endorsementKey = tpmBridge->GetEndorsementKey();
+}
+
+std::string MainPage::DpsEnrollmentInfo::ToJsonString() const
+{
+    string s;
+
+    s += "{";
+    s += "\"regid\":\"";
+    s += WideToMultibyte(registrationId->Data());
+    s += "\",\"ek\":\"";
+    s += WideToMultibyte(endorsementKey->Data());
+    s += "\"";
+    s += "}";
+
+    return s;
+}
+
+std::string MainPage::DpsEnrollmentInfo::ToSvgString() const
+{
+    const QrCode::Ecc errCorLvl = QrCode::Ecc::LOW;
+    const QrCode qr = QrCode::encodeText(ToJsonString().c_str(), errCorLvl);
+    return qr.toSvgString(4 /*border*/);
+}
+
+wstring MainPage::WriteLocalFile(
+    const string& content,
+    const wstring& fileName)
+{
+    auto filePath = wstring(Windows::Storage::ApplicationData::Current->LocalFolder->Path->Data()) + L"\\" + fileName;
+    HANDLE fileHandle = CreateFile2(filePath.c_str(), GENERIC_WRITE, 0, CREATE_ALWAYS, nullptr);
+    if (fileHandle != INVALID_HANDLE_VALUE)
+    {
+        DWORD bytesWritten;
+        auto writeResult = WriteFile(fileHandle, content.data(), static_cast<DWORD>(content.length()), &bytesWritten, nullptr);
+        if (writeResult == FALSE || bytesWritten != content.length())
+        {
+            throw runtime_error("Failed to write content message to the content file: error code " + to_string(GetLastError()));
+        }
+        CloseHandle(fileHandle);
+    }
+
+    return filePath;
+}
+
+void MainPage::OnReadTPMInfo(
+    Platform::Object^ sender,
+    Windows::UI::Xaml::RoutedEventArgs^ e)
+{
+    // Load the data from the TPM...
+    DpsEnrollmentInfo info;
+    info.Load();
+
+    // Load the data in the UI...
+    RegIdBox->Text = info.registrationId;
+    EKBox->Text = info.endorsementKey;
+
+    // Save it to an image...
+    wstring fullFileName = WriteLocalFile(info.ToSvgString(), L"qrd.svg");
+
+    // Load the image in the UI...
+    Uri^ uri = ref new Uri(ref new Platform::String(fullFileName.c_str()));
+    SvgImageSource^ svgImageSource = ref new SvgImageSource(uri);
+    QrImage->Source = svgImageSource;
+}
---
+++ samples/e2e.cpp.demo/MainPage.xaml.h
@@ -0,0 +1,54 @@
+﻿/*
+Copyright 2018 Microsoft
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+and associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH
+THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#pragma once
+
+#include "MainPage.g.h"
+
+namespace e2e_cpp_demo
+{
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public ref class MainPage sealed
+	{
+        struct DpsEnrollmentInfo
+        {
+            Platform::String^ registrationId;
+            Platform::String^ endorsementKey;
+
+            void Load();
+
+            std::string ToJsonString() const;
+            std::string ToSvgString() const;
+        };
+
+    public:
+        MainPage();
+
+    private:
+        void OnReadTPMInfo(
+            Platform::Object^ sender,
+            Windows::UI::Xaml::RoutedEventArgs^ e);
+
+        std::wstring WriteLocalFile(
+            const std::string& content,
+            const std::wstring& fileName);
+
+        static DpsEnrollmentInfo GetDpsEnrollmentInfo();
+
+        Windows::Foundation::IAsyncOperation<Windows::Storage::StorageFile^>^ _sampleFile;
+    };
+}
